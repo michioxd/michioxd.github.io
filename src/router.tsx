ce7e497e61f03806a7ed6109b103d8feea06035c
michioxd <michio.haiyaku@gmail.com>
2023-12-11 17:13:24 +0700

    404

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import PageHome from "./pages/Home";
 import PageOSLicense from "./pages/OSLicense";
 import PagePortfolio from "./pages/Portfolio";
+import PageNotFound from "./pages/NotFound";
 
 export default function HomeRouter() {
     return (
@@ -9,6 +10,7 @@ export default function HomeRouter() {
             <Route index element={<PageHome />} />
             <Route path="opensource-license" element={<PageOSLicense />} />
             <Route path="portfolio" element={<PagePortfolio />} />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     )
 }
