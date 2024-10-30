b9e9939d9eb74bd405e324e244d73d1e6d84ec04
michioxd <michio.haiyaku@gmail.com>
2024-10-30 18:44:38 +0700

    lmao

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
                 <img srcSet={logo} alt="logo" className={cls.Logo} />
                 <div className={cls.Content}>
                     <Text size="1" color="gray">
-                        Hoshino Takanashi art by <Link color="gray" href="https://x.com/kurun_p" target="_blank">くるん</Link>
+                        Hoshino Takanashi art by <Link color="gray" href="https://x.com/kurun_p" target="_blank">くるん</Link>. Website created with <Link color="gray" href="https://vite.dev/guide/#scaffolding-your-first-vite-project" target="_blank">Vite + React</Link>.
                     </Text>
                     <Text size="1" color="gray">
                         &copy; 2024 michioxd. <Link color="gray" href="https://github.com/michioxd/michioxd.github.io" target="_blank">Available under the CC BY-NC-ND 4.0 license</Link>.
